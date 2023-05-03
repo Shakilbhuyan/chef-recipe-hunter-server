0a6a3d5346ee320b8794d5750532ad5c4643f752
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-05-03 11:35:43 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 4000;
 
 const chef = require('./Data/chef.json');
 
+app.use(cors());
 app.get('/', (req, res)=>{
     res.send('The Chef Server is Onnnnn')
 } );
