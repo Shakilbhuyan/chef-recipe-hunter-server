2356d17232e5939d33a5e82a5ccfbaabd3b5fa3a
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-05-03 11:28:31 +0600

    passing data complete

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+const chef = require('./Data/chef.json');
+
 app.get('/', (req, res)=>{
     res.send('The Chef Server is Onnnnn')
 } );
 
+app.get('/chef', (req, res)=>{
+    res.send(chef);
+})
 app.listen(port, ()=>{
     console.log(`The port is ${port}`)
 })
